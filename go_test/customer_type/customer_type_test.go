f281508ab95c6d9d6d6aa2e482c6303f9e8b1bfd
Longxr <longxuanren@gmail.com>
2020-04-08 23:33:24 +0800

    Update customer_type_test.go

--- go_test/customer_type/customer_type_test.go
+++ go_test/customer_type/customer_type_test.go
@@ -26,3 +26,31 @@ func TestFn(t *testing.T) {
 	tsSF := timeSpent(slowFun)
 	t.Log(tsSF(10))
 }
+
+type student struct {
+	name string
+	age  int
+}
+
+func TestRangeStruct(t *testing.T) {
+	m := make(map[string]*student)
+	stus := []student{
+		{name: "小王子", age: 18},
+		{name: "娜扎", age: 23},
+		{name: "大王八", age: 9000},
+	}
+
+	for i := 0; i < len(stus); i++ {
+		stu := stus[i]
+		m[stu.name] = &stu
+		fmt.Printf("stu address: %p, name: %v\n", &stu, stu.name)
+	}
+
+	// for _, stu := range stus {
+	// 	m[stu.name] = &stu
+	// 	fmt.Printf("stu address: %p, name: %v\n", &stu, stu.name)
+	// }
+	for k, v := range m {
+		fmt.Println(k, "=>", v.name)
+	}
+}
